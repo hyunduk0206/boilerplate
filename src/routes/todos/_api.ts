343e44175473992aee795908065aadb118b855b5
comverser <hyunduk0206@gmail.com>
2022-02-25 02:54:22 +0000

    Add PATCH endpoint - update text.

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -3,7 +3,7 @@ import type { Request } from '@sveltejs/kit';
 // TODO: Persist in database
 let todos: Todo[] = [];
 
-export const api = (request: Request, todo?: Todo) => {
+export const api = (request: Request, data?: Record<string, unknown>) => {
 	let status = 500;
 	let body = {};
 
@@ -13,12 +13,21 @@ export const api = (request: Request, todo?: Todo) => {
 			body = todos;
 			break;
 		case 'POST':
-			todos.push(todo);
-			body = todo;
+			todos.push(data as Todo);
+			body = data;
 			status = 201;
 			break;
 		case 'DELETE':
-			todos = todos.filter((todo) => todo.uid !== request.params.uid);
+			todos = todos.filter((data) => data.uid !== request.params.uid);
+			status = 200;
+			break;
+		case 'PATCH':
+			todos = todos.map((todo) => {
+				if (todo.uid === request.params.uid) {
+					todo.text = data.text as string;
+				}
+				return todo;
+			});
 			status = 200;
 			break;
 
