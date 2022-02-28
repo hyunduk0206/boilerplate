7c5bab5aef40154d1fd673a8bfa3cadc567090f5
comverser <hyunduk0206@gmail.com>
2022-02-28 01:05:01 +0000

    Debug null error on patch request.

--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -7,7 +7,7 @@ export const del: RequestHandler = (request) => {
 
 export const patch: RequestHandler<Request, FormData> = (request) => {
 	return api(request, {
-		text: request.body.get('text'),
+		text: request.body.has('text') ? request.body.get('text') : undefined,
 		done: request.body.has('done') ? !!request.body.get('done') : undefined
 	});
 };
